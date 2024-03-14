ecc44e1d250ed2f182e0bb43affc9328b1ecc8be
Ashwin Ram <ashwinsundarram@gmail.com>
2024-03-14 13:35:17 +0530

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,21 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { getAllPasswords, savePassword } from "./service/service";
 import { validateRequest } from "./handler/requestHandler";
+import cors from "cors";
 
 const app: Express = express();
 dotenv.config();
 const port: number | string = process.env.PORT || 8090;
 app.use(express.json());
+app.use(cors());
 
 app.get("/get-all", (req: Request, res: Response) => {
+  console.log("Get all passwords called");
   res.status(200).send(getAllPasswords());
 });
 
 app.post("/save", (req: Request, res: Response) => {
+  console.log("Save password called");
   if (validateRequest(req)) {
     const saved: object = savePassword(req.body);
     res.status(201).send(saved);
